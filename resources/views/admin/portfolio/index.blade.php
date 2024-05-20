eb7671156350b211accf0ebee11ee3a99d75954e
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-05-20 15:59:01 +0200

    add Index Method Commit

--- resources/views/admin/portfolio/index.blade.php
+++ resources/views/admin/portfolio/index.blade.php
@@ -1,6 +1,47 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="container py-3 bg-dark text-white">
-        <p>Benvenuto</p>
-    </div>
-@endsection
+    <header class="py-3 bg-dark text-white">
+        <div class="container d-flex align-items-center justify-content-between">
+            <h1>
+                Projects :
+            </h1>
+            {{--  <a class="btn btn-primary" href="{{ route('admin.posts.create') }}"> New Post</a> --}}
+        </div>
+    </header>
+
+    <div class="container">
+
+        <div class="table-responsive">
+            <table class="table table-light">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">link</th>
+                        <th scope="col">ACTIONS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($projects as $project)
+                        {{--  @dd($projects) --}}
+                        <tr class="">
+                            <td scope="row">{{ $project->id }}</td>
+                            <td scope="row">{{ $project->title }}</td>
+                            <td scope="row">{{ $project->slug }}</td>
+                            <td scope="row">{{ $project->link }}</td>
+
+                            <td>
+                                {{-- <a class="btn btn-dark" href="{{ route('admin.projects.show', $project) }}"> --}} &RightArrow;
+                                View</a>/Modify/Delete
+                            </td>
+                        </tr>
+                    @empty
+                        <h3>No projects yet !</h3>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+
+        {{ $projects->links('pagination::bootstrap-5') }}
+    @endsection
